ad61f3976335c8d43f7f8ba469e16970989fe678
beforged <richardyu042@gmail.com>
2018-07-02 23:10:20 -0400

    started ruby impl

---
+++ parse.rb
@@ -0,0 +1,19 @@
+Class in
+    #maybe initalize some object we can serialize into a JSON to convert to python?
+#use our already writen python framework
+    def intialize()
+    end
+
+   def load(filename)
+        file = IO,readlines(filename, "\n")
+        if file = []
+            return false
+        end
+        #this chomps the \n at the end of the line
+        file.each do |chomp|
+            chomp.chomp!
+        end
+        file.each do |proc|
+            if proc.match? 
+   end
+end
